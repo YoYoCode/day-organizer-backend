8a7ab0050771c5758894719b1f5339566586231f
Harish Matta <harish.matta31@gmail.com>
2020-05-27 00:03:50 +0530

    removing redis connection step for deployment

--- src/services/index.js
+++ src/services/index.js
@@ -1,11 +1,10 @@
 const webPusher = require('./push-notifications.js');
-const redisClient = require('./redis.js');
+// const redisClient = require('./redis.js');
 
 module.exports = async config => {
   try {
-    console.log('<<>>', config);
     await webPusher.configure(config);
-    await redisClient.configure(config);
+    // await redisClient.configure(config);
   } catch (error) {
     console.log(error);
     throw error;
