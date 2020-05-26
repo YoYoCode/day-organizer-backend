df8806b1e03125451672a020325327a8606e5c1c
Harish Matta <harish.matta31@gmail.com>
2020-05-26 23:46:19 +0530

    [NOTIFS] <Matta> | Added env for production.

--- src/config/env/production.js
+++ src/config/env/production.js
@@ -5,9 +5,10 @@
 module.exports = {
   DB_URL: process.env.MONGO_DB_URL,
   DEFAULT_DB: process.env.DEFAULT_DB,
-  vapid: {
-    public_vapid_key: process.env.PUBLIC_VAPID_KEY,
-    private_vapid_key: process.env.PRIVATE_VAPID_KEY
+  VAPID_KEYS: {
+    PUBLIC_VAPID_KEY: process.env.PUBLIC_VAPID_KEY,
+    PRIVATE_VAPID_KEY: process.env.PRIVATE_VAPID_KEY,
+    MAIL_TO: 'mailto:codingknights@gmail.com'
   },
   api: {
     bodySizeLimit: process.env.API_BODY_SIZE_LIMIT,
