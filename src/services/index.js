77b9f5a979c27b42ba8f4c4e36faab3c44192619
Harish Matta <harish.matta31@gmail.com>
2020-05-27 00:00:19 +0530

    fixing heroku deployment

--- src/services/index.js
+++ src/services/index.js
@@ -3,6 +3,7 @@ const redisClient = require('./redis.js');
 
 module.exports = async config => {
   try {
+    console.log('<<>>', config);
     await webPusher.configure(config);
     await redisClient.configure(config);
   } catch (error) {
